556f2583fb25022c4b0632ba49caecd2a606f38c
Kiran <kiranpuri532@gmail.com>
2023-07-14 08:20:21 +0530

    [Kiran]Add:\ sad mood should return sad message

---
+++ MoodAnalyserProblem/MoodAnalyserProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class Program
+    {
+        // Nunit test is used to test the program and check the values are correct or not
+        static void Main(string[] args)
+        {
+            //DLL- Dynamic Link Library that contain functions and code that can
+            //be used by more than one program at a time.
+        }
+    }
+}
---
+++ MoodAnalyserProblem/MoodAnalyserTestFile/MoodAnaluse_test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoodAnalyserProblem;
+
+namespace MoodAnalyserTestFile
+{
+    public class MoodAnaluse_test
+    {
+        MoodAnalyse moodAnalyse = new MoodAnalyse();
+        [Test]
+        public void GivenSadMood_ShouldReturnSad()
+        {
+            string result = moodAnalyse.AnalyseMood("I am in Sad Mood");
+            Assert.AreEqual(result, "Sad");
+        }
+    }
+}
+
